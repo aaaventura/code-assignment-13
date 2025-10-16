d0170cf4d007757e8b725554c1be26be90251f59
aaaventura <aventura39@rrc.ca>
2025-10-16 15:21:40 -0500

    Update TableCell.types.tsx

--- src/components/Table/TableCell/TableCell.types.tsx
+++ src/components/Table/TableCell/TableCell.types.tsx
@@ -1,17 +1,6 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import TableCell from './TableCell';
-
-describe('TableCell Component', () => {
-  test('renders table cell and is visible', () => {
-    render(<table><tbody><tr><TableCell>Cell Content</TableCell></tr></tbody></table>);
-    const cellElement = screen.getByRole('cell');
-    expect(cellElement).toBeVisible();
-  });
-
-  test('changes background color when disabled', () => {
-    render(<table><tbody><tr><TableCell disabled>Cell Content</TableCell></tr></tbody></table>);
-    const cellElement = screen.getByRole('cell');
-    expect(cellElement).toHaveStyle('background-color: #cccccc');
-  });
-});
+export interface TableCellProps {
+  children?: React.ReactNode;
+  disabled?: boolean;
+  bgColor?: string;
+  [key: string]: any;
+}
