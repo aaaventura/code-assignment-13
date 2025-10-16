25fc80f9d7c7359dca0872bf12410ccc76fbf8be
aaaventura <aventura39@rrc.ca>
2025-10-16 15:14:15 -0500

    Update TableFooter.test.tsx

--- src/components/Table/TableFooter/TableFooter.test.tsx
+++ src/components/Table/TableFooter/TableFooter.test.tsx
@@ -4,14 +4,30 @@ import TableFooter from './TableFooter';
 
 describe('TableFooter Component', () => {
   test('renders table footer and is visible', () => {
-    render(<table><TableFooter><tr><td>Footer</td></tr></TableFooter></table>);
+    render(
+      <table>
+        <TableFooter>
+          <tr>
+            <td>Footer</td>
+          </tr>
+        </TableFooter>
+      </table>
+    );
     const footerElement = screen.getByText('Footer');
     expect(footerElement).toBeVisible();
   });
 
   test('changes background color when disabled', () => {
-    render(<table><TableFooter disabled><tr><td>Footer</td></tr></TableFooter></table>);
-    const footerElement = screen.getByText('Footer');
-    expect(footerElement).toHaveStyle('background-color: #cccccc');
+    const { container } = render(
+      <table>
+        <TableFooter disabled>
+          <tr>
+            <td>Footer</td>
+          </tr>
+        </TableFooter>
+      </table>
+    );
+    const tfootElement = container.querySelector('tfoot');
+    expect(tfootElement).toHaveStyle('background-color: #cccccc');
   });
 });
